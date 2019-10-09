93514a2e186d468b0ca38f280c3e1e7a27824b3c
Alp Ozkan <26024533@A00102586>
2019-10-09 16:00:05 +0300

    finishing touches for backend

--- Backend/polls/src/main/java/com/example/polls/repository/VoteRepository.java
+++ Backend/polls/src/main/java/com/example/polls/repository/VoteRepository.java
@@ -2,15 +2,16 @@ package com.example.polls.repository;
 
 import com.example.polls.model.ChoiceVoteCount;
 import com.example.polls.model.Vote;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface VoteRepository {
+public interface VoteRepository extends JpaRepository<Vote, Long> {
     @Query("SELECT NEW com.example.polls.model.ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v WHERE v.poll.id in :pollIds GROUP BY v.choice.id")
     List<ChoiceVoteCount> countByPollIdInGroupByChoiceId(@Param("pollIds") List<Long> pollIds);
 
@@ -28,6 +29,4 @@ public interface VoteRepository {
 
     @Query("SELECT v.poll.id FROM Vote v WHERE v.user.id = :userId")
     Page<Long> findVotedPollIdsByUserId(@Param("userId") Long userId, Pageable pageable);
-
-
 }
